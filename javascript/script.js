241d59d808fb0352adbd5e4c9a9f0f7622958ef4
佐藤渚 <48896484+HimenoNatuki@users.noreply.github.com>
2022-11-18 16:24:26 +0900

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+
+
+console.log("Start-up");
+
+function tapone() {
+    document.cookie = 'address=Tokyo';
+    document.cookie = 'try=one';
+}
+
+function push_button1() {
+    console.log(window.navigator.userAgent);
+    alert('push button!');
+}
